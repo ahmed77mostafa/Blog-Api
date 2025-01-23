d7c5ebadac83fec002efa3b8fb7d0d4d4f7a7577
Ahmed mostafa <Ahmed mostafa@DESKTOP-BJQ2RLS>
2025-01-23 12:07:54 -0800

    Define IComment for Comment repo

--- Blog/Repositories/Implements/CommentRepository.cs
+++ Blog/Repositories/Implements/CommentRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace Blog.Repositories.Implements
+﻿using Blog.DTOs;
+using Blog.Repositories.Interfaces;
+
+namespace Blog.Repositories.Implements
 {
-    public interface CommentRepository
+    public class CommentRepository : ICommentRepository
     {
+        public void AddComment(CommentDto commentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCommentPost(PostCommentDto postCommentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCommentPostUser(PostCommentUserDto postCommentUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCommentToPost(int postId, CommentDto commentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteComment(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PostCommentUserDto GetCommentById(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PostCommentUserDto> GetComments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateComment(int commentId, CommentDto commentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommentPost(int commentId, PostCommentDto commentPostDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
